7402480048cc9d341a746b69583bec384ca06b5b
lapsio <lapsio3@gmail.com>
2015-03-28 16:21:51 +0100

    objects header rev0

--- include/object.h
+++ include/object.h
@@ -6,9 +6,17 @@
 class Object : public PhysicalBody, public LightSource, public Decal
 {
 private:
-  std::string name;
+  static int maxId;
+  const std::string name;
+  int id;
 public:
-  Object(const char* name);
+  Object(const std::string name=std::string("Object"),
+         const LightSource&=LightSource(),
+         const PhysicalBody&=PhysicalBody());
+  Object(const Object& ref);
+  virtual ~Object();
+
+  virtual int typeOf();
 };
 
 class Decal
@@ -16,6 +24,7 @@ class Decal
 private:
 public:
   Decal();
+  virtual ~Decal(){};
 };
 
 class LightSource
@@ -23,15 +32,16 @@ class LightSource
 private:
 public:
   LightSource();
+  virtual ~LightSource(){};
 };
 
 class PhysicalBody
 {
 private:
-  XyPair * boundBox = nullptr;
+  XyPair * boundBox;
   struct Array
-      *collisionMesh = nullptr,
-      *collisionNormals = nullptr;
+      *collisionMesh,
+      *collisionNormals;
 
   enum ObjectType {passive='p', dynamic='d'} type;
   enum CollisionType {ghost='g',solid='s'} collisionType;
@@ -41,8 +51,25 @@ private:
 
 public:
   PhysicalBody();
-  XyVector * isColliding(PhysicalBody B);
+  virtual ~PhysicalBody();
+
   void reshape(struct Array mesh);
+};
+
+
+class PhysicsEngine
+{
+private:
+  GameMap* map;
+
+  void moveObjects(int objIndex=-1);
+  void moveAnchors();
+
+  void collideObjects(int objIndex=-1);
+
+  void postMotion();
+  inline void postMotion(int objIndex);
+
 };
 
 #endif // OBJECT_H
