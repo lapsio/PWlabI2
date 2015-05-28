d98d8959a1f742219b0d100105dea26595ad534f
lapsio <lapsio3@gmail.com>
2015-05-28 05:15:09 +0200

    c

--- include/GameDomain.h
+++ include/GameDomain.h
@@ -7,6 +7,13 @@
 
 class GameSession;
 
+/**
+ * @brief The BaseEvent class class providing events functionality designed for session management. Drivers can affect session flow by returning special Events.
+ * Base event doesn't contain data, however drivers can create extended Event providing for example new session to be loaded.
+ *
+ * @see Event
+ */
+
 class BaseEvent
 {
 
@@ -16,8 +23,8 @@ class BaseEvent
             none = 0,
             swapSessions
         };
-        const Type type;
-        const bool interrupt;
+        const Type type; /**< event type */
+        const bool interrupt; /**< whether to interrupt drivers execution queue and jump to session loop in order to instantly parse Event */
 
     public:
 
@@ -32,11 +39,15 @@ class BaseEvent
 #define Event_H
 
 template <typename D>
+/**
+ * @brief The Event class extension to BaseEvent class allowing drivers to pass custom data in event.
+ * @see BaseEvent
+ */
 class Event: public BaseEvent
 {
 
     public:
-        const D data;
+        const D data; /**< custom data */
 
         Event (const D data, const Type t = Type::none, const bool interrupt = false) : BaseEvent (t,interrupt), data(data){}
         virtual ~Event(){}
@@ -48,6 +59,10 @@ class Event: public BaseEvent
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
+/**
+ * @brief The Interface class Interface to be implemented by all drivers. Each driver needs to implement certain functionality in order to work with
+ * game domain manager and session properly. Interface by default inherits from GCRef so it's deleted automatically once all sessions using driver will be destroyed
+ */
 
 class Interface: public GCRef
 {
@@ -57,6 +72,11 @@ class Interface: public GCRef
 
         virtual ~Interface();
 
+        /**
+         * @brief run method called before each screen redraw
+         * @param session current session passed to driver by game domain manager
+         * @return
+         */
         virtual BaseEvent& run(GameSession& session) = 0;
 
 };
@@ -66,6 +86,10 @@ class Interface: public GCRef
 #ifndef GAMEDOMAIN_H
 #define GAMEDOMAIN_H
 
+/**
+ * @brief The GameDomain class Game domain manager class, providing game domain code layer for game logic. Game is created using multiple drivers, handling
+ * various tasks.
+ */
 
 class GameDomain
 {
@@ -81,10 +105,26 @@ class GameDomain
         GameDomain();
         ~GameDomain();
 
-        void add (Interface&);
-        void remove (Interface*);
+        /**
+         * @brief add add new driver to execution queue of this manager
+         * @param interface interface to be added
+         */
+        void add (Interface& interface);
+        /**
+         * @brief remove remove driver from manager
+         * @param interface to be removed
+         */
+        void remove (Interface* interface);
+        /**
+         * @brief bindSession bind session to this manager. THIS IS REQUIRED. Each manager HAS TO have some session bound to it.
+         * @param s session to be bound
+         */
         inline void bindSession(GameSession& s){this->session=&s;}
 
+        /**
+         * @brief reload reload drivers queue
+         * @return
+         */
         Chain <BaseEvent&>* reload ();
 
 };
