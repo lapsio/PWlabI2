4d4a8845de0a7273ddf26ddc4f14cff7435d7e49
lapsio <lapsio3@gmail.com>
2015-05-28 05:26:31 +0200

    docs

--- include/engine.h
+++ include/engine.h
@@ -5,6 +5,10 @@
 #include "include/gfx.h"
 #include "drivers/include/EngineStart.h"
 
+/**
+ * @brief The GameEngine class main game engine class
+ */
+
 class GameEngine
 {
 private:
@@ -15,7 +19,15 @@ public:
   GameEngine();
   ~GameEngine();
 
+  /**
+   * @brief start start new game
+   * @param interruptTrigger reference to bool triggering extraordinary execution interruption
+   */
   void start(bool& interruptTrigger);
+  /**
+   * @brief purgeSession delete specified session (sned it to the mooooon)
+   * @param session session to be deleted
+   */
   void purgeSession(GameSession* session);
 };
 
--- include/gfx.h
+++ include/gfx.h
@@ -4,11 +4,15 @@
 #include "include/object.h"
 #include "SFML/Graphics.hpp"
 
+/**
+ * @brief The RenderEngine class rendering engine for game
+ */
+
 class RenderEngine {
 public:
-  static constexpr int UNITSIZE=10;
+  static constexpr int UNITSIZE=10; /**< size of single session-wise unit in px*/
 
-  static constexpr int defaultBufferSize=256;
+  static constexpr int defaultBufferSize=256; /**< default draw buffer size*/
   static constexpr char* defaultWindowName=(char*)"Game window";
 
   struct objRecord {
@@ -29,6 +33,13 @@ private:
   const bool resizable;
 
 public:
+  /**
+   * @brief RenderEngine basic constructor
+   * @param objBufferSize initial size of draw buffer (max number of rendered Decals)
+   * @param autoresize whether buffer should auto resize
+   * @param resizeable whether buffer should allow manual resizing
+   * @param title window title
+   */
   RenderEngine(int objBufferSize=RenderEngine::defaultBufferSize,
                bool autoresize=false,
                bool resizeable=true,
@@ -40,15 +51,42 @@ public:
 
   ~RenderEngine();
 
+  /**
+   * @brief pushObject add new Decal to render queue
+   * @param position position of decal to be rendered (in session-wise units)
+   * @param decal decal to be rendered
+   */
   void pushObject(const PointXY position, const Decal& decal);
+  /**
+   * @brief clear clear draw buffer
+   */
   void clear();
+  /**
+   * @brief flush flush draw buffer
+   */
   void flush();
 
+  /**
+   * @brief centerCamera center camera on specified point
+   * @param pos new camera center point (provided in session-wise units)
+   */
   void centerCamera(const PointXY pos);
   inline void centerCamera(int W, int H){this->centerCamera(PointXY(W,H));}
-  void canvasResize(const PointXY pos);
+  /**
+   * @brief canvasResize resize virtual canvas
+   * @param pos new size of canvas
+   */
+  void canvasResize(const PointXY size);
   inline void canvasResize(int W, int H){this->canvasResize(PointXY(W,H));}
+  /**
+   * @brief resizeDrawBuffer resize draw buffer
+   * @param maxObjCount new max objects count
+   */
   void resizeDrawBuffer(int maxObjCount);
+  /**
+   * @brief setWindowTitle set window title
+   * @param title
+   */
   void setWindowTitle(const char* title);
 };
 
